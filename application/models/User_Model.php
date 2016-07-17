52ff80fc32f75347b0f344ac82ff0ea93eaaba17
vrburgos <vrburgos@live.com>
2016-07-16 19:31:39 -0600

    first commit
    adding CI

---
+++ application/models/User_Model.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class User_model extends CI_Model {
+
+	
+
+	public function __construct()
+	{
+		parent::__construct();
+		
+	}
+
+}
+
+/* End of file User_Model.php */
+/* Location: ./application/models/User_Model.php */
